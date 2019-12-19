f57e06321f98575424b9ff9d71516eea4f72042a
Carazo Fernández <icarazo@minsait.com>
2019-12-19 15:02:53 +0100

    Pokemon service & pokemonDisplay

---
+++ src/app/pokemon.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http' 
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getPokemon(id:string):Observable<Object>{
+    return this.httpClient.get("https://pokeapi.co/api/v2/pokemon/"+id)
+  }
+}
