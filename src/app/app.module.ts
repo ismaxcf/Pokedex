f29a0d167d9b2155c712eee609f8fded445958d5
Orgaz Portero <corgaz@minsait.com>
2019-12-20 11:23:52 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ListComponent } from './list/list.component';
-=======
->>>>>>> 737ec6c95fc4f354e4d59f70b0d117620d850252
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { PokemonDisplayComponent } from './pokemon-display/pokemon-display.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,10 +14,7 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     ListComponent,
-=======
->>>>>>> 737ec6c95fc4f354e4d59f70b0d117620d850252
     LandingpageComponent,
     PokemonDisplayComponent
 
