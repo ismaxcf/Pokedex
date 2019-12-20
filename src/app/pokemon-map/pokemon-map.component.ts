dbd1ca61cac6d62b3fb99b5db2150e5a5717a5f0
Sánchez Pérez <msanchezpe@minsait.com>
2019-12-20 10:29:38 +0100

    Cambios Leaflet variable

--- src/app/pokemon-map/pokemon-map.component.ts
+++ src/app/pokemon-map/pokemon-map.component.ts
@@ -12,12 +12,12 @@ export class PokemonMapComponent implements OnInit {
 
   ngOnInit() {
     let options = {attributionControl: false};
-    var mymap = L.map('mymap',options).setView([40.425941, -3.565470], 6);
+    var myMap = L.map('myMap',options).setView([40.425941, -3.565470], 6);
     var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     });
-    Esri_WorldImagery.addTo(mymap)
-    let marker = L.marker([40.425941, -3.565470]).bindPopup('Esto es <br> <b>COSLADA</b>')
-    marker.addTo(mymap)
+    Esri_WorldImagery.addTo(myMap)
+    let marker = L.marker([40.425941, -3.565470]).bindPopup('COSLADA')
+    marker.addTo(myMap)
   }
 }
