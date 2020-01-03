1ff2f3d97da245ccf2c22be55ca21815941af87d
Carazo Fernández <icarazo@minsait.com>
2020-01-03 11:53:11 +0100

    spect conflicts

--- src/app/pokedex/pokedex.component.spec.ts
+++ src/app/pokedex/pokedex.component.spec.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-
-import { PokedexComponent } from './pokedex.component'
-
-describe('PokedexComponent', () => {
-  let component: PokedexComponent
-  let fixture: ComponentFixture<PokedexComponent>
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [PokedexComponent],
-    }).compileComponents()
-  }))
-=======
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { PokedexComponent } from "./pokedex.component";
@@ -26,7 +11,6 @@ describe("PokedexComponent", () => {
       declarations: [PokedexComponent]
     }).compileComponents();
   }));
->>>>>>> 726fc73ba7c908f0992f19db3bb6fcf943e8e5d0
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PokedexComponent)
@@ -34,14 +18,7 @@ describe("PokedexComponent", () => {
     fixture.detectChanges()
   })
 
-<<<<<<< HEAD
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
-})
-=======
   it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
->>>>>>> 726fc73ba7c908f0992f19db3bb6fcf943e8e5d0
--- src/app/pokemon-map/pokemon-map.component.spec.ts
+++ src/app/pokemon-map/pokemon-map.component.spec.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-
-import { PokemonMapComponent } from './pokemon-map.component'
-
-describe('PokemonMapComponent', () => {
-  let component: PokemonMapComponent
-  let fixture: ComponentFixture<PokemonMapComponent>
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [PokemonMapComponent],
-    }).compileComponents()
-  }))
-=======
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { PokemonMapComponent } from "./pokemon-map.component";
@@ -26,7 +11,6 @@ describe("PokemonMapComponent", () => {
       declarations: [PokemonMapComponent]
     }).compileComponents();
   }));
->>>>>>> 726fc73ba7c908f0992f19db3bb6fcf943e8e5d0
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PokemonMapComponent)
@@ -34,14 +18,7 @@ describe("PokemonMapComponent", () => {
     fixture.detectChanges()
   })
 
-<<<<<<< HEAD
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
-})
-=======
   it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
->>>>>>> 726fc73ba7c908f0992f19db3bb6fcf943e8e5d0
